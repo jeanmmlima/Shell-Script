eb96c530c4da86b44b17ac37a4a5387ab66f58df
jeanmmlima <jeanmariomlima@gmail.com>
2019-02-21 14:37:58 -0300

    mysql installer

--- installers/packdevweb.sh
+++ installers/packdevweb.sh
@@ -16,3 +16,19 @@
 sudo apt-get update
 sudo apt-get install apache2
 
+#Para testar:
+#abrir o navegador e digitar: http://localhost
+
+
+#MySQL
+
+sudo apt-get install mysql-server
+
+#para modificacao basica de 
+#parametros de segunrança
+#descomente a proxima linha 
+
+# sudo mysql_secure_installation
+
+# PHP
+
