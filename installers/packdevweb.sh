f6c7ee439a2e4a421c5d10fcded038ac7eecf15f
jeanmmlima <jeanmariomlima@gmail.com>
2019-02-21 14:31:19 -0300

    apache2 installer

---
+++ installers/packdevweb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#########################################
+
+	# SCRIPT PARA INSTALACAO:
+	# 	- APACHE 2
+	#   - MySQL
+	#   - PHP 7
+
+	#  DEVELOPED BY: JEAN MARIO M LIMA
+
+##########################################
+
+
+#APACHE
+sudo apt-get update
+sudo apt-get install apache2
+
